f8296bb7145b88560d8bc8ad8b59600e6aa493ab
Stasy- st4 <ishtubaevaa@gmail.com>
2024-10-17 17:11:19 +0500

    homework1

---
+++ main.py
@@ -0,0 +1,13 @@
+#1st program
+print(9 ** 0,5)
+#2nd program
+print(9,99 >= 9,98 or 1000 != 1000>1)
+#3rd program
+print(2 * 2 + 2)
+print(2 * (2 + 2))
+print((2 * 2 + 2) == (2 * (2 + 2)))
+#4st program
+print('123.456')
+print(type(float('123.456')))
+print(123.456 * 10)
+print(type(int(123.456 * 10)))
